c95fbb5ab1beea92420166ba7e76ff86484855f4
Tom Bauer <tbauer1898@gmail.com>
2021-03-26 18:21:28 -0500

    basic file structure and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
+import Header from './Header';
+import Favorites from './Pages/Favorites';
+import About from './Pages/About';
+import Landing from './Pages/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <h1>Tom Bauer</h1>
+
+        <Switch>
+          <Route path='/favorites'>
+            <Favorites />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/'>
+            <Landing />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Favorites.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Favorites = () => {
+    return(
+        <div>
+            <h2>ThESE ARE a few OF MY FAVORITE THINGS</h2>
+        </div>
+    )
+}
+
+export default Favorites
